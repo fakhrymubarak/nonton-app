796aa6c902d8fe7ed53390a02eca536b90885c45
Fakhry Mubarak <fachrymubarak@gmail.com>
2020-01-22 02:18:10 +0700

    ini adalah latihan commit and push dengan memberi note pada halaman pertama syntax

--- app/src/main/java/com/fakhry/nonton/SplashScreenActivity.kt
+++ app/src/main/java/com/fakhry/nonton/SplashScreenActivity.kt
@@ -6,6 +6,16 @@ import android.os.Bundle
 import android.os.Handler
 import com.fakhry.nonton.onboarding.OnboardingOneActivity
 
+/*
+* Ini adalah halaman pertama yang akan terbuka
+* Tidak ada fitur khsus disini, selain menahan
+* aplikasi terbuka untuk beberapa detik.
+*
+* proses yang perlu dilakukan untuk melakukan commit
+* 1. pastikan code kalian sudah up to date
+* 2. tekan centang hijau yang berada di pojok kanan atas (commit)
+* */
+
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +28,6 @@ class SplashScreenActivity : AppCompatActivity() {
                 OnboardingOneActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 2000)
     }
 }
